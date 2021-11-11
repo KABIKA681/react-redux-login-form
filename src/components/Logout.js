e3649461261fd7617f354bb31da7eca4f19e8b27
JAMAN <pascalkabikampan@gmail.com>
2021-11-11 10:52:13 +0200

    second-commit

---
+++ src/components/Logout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Logout() {
+    return (
+        <div className="logout">
+            <h1>Welcome <span className="user__name">Pascal</span>
+            <button className="logout__btn">Logout</button>
+            </h1>
+        </div>
+    )
+}
+
+export default Logout
